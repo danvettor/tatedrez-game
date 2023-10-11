39243464ec196d99b51a7ae82becc78d7ce78a70
Danilo F. Vettorazzi <danfvettor@gmail.com>
2023-10-11 20:06:46 -0300

    First version of pieces selection UI

---
+++ Assets/PiecesUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PiecesUI : MonoBehaviour
+{
+    [SerializeField] private PlayerColor _playerColor;
+    [SerializeField] private Button _rook;
+    [SerializeField] private Button _bishop;
+    [SerializeField] private Button _knight;
+
+    void Start()
+    {
+        
+    }
+
+}
